24fee6346a7779b3c108117be7bae9d06f2cdf25
吉村直人 <ahowateto123@gmail.com>
2025-09-23 10:09:55 +0900

    module: cloudfront

---
+++ modules/cloudfront/variable.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "root_domain" {
+  type = string
+}
+
+variable "cdn_public_key" {
+  type = string
+}
+
+variable "cdn_bucket_id" {
+  type = string
+}
+
+variable "cdn_bucket_domain_name" {
+  type = string
+}
