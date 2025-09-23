8265ac9de7c367fe60fbd2728c0ac3996c59339d
吉村直人 <ahowateto123@gmail.com>
2025-09-23 11:39:21 +0900

    module: EventBridge

---
+++ modules/eventbridge/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "stop_comfyui_lambda_function_name" {
+  type = string
+}
