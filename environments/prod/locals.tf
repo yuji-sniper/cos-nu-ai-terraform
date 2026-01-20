e64493134019612f26e427c704f771e699d728c8
吉村直人 <ahowateto123@gmail.com>
2026-01-20 22:44:04 +0900

    chore: Remove obsolete Terraform configuration files for production environment to clean up the repository

--- environments/prod/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  env = "prod"
-  availability_zones = [
-    "us-east-1a"
-  ]
-}
--- environments/prod/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "vercel_cname_target" {
-  type = string
-}
-
-variable "media_private_public_key" {
-  type = string
-}
